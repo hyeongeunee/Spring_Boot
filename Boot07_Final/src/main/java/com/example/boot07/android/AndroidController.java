4f3446cc6709bc3c1be0683691c6a95fcc76f17f
hyeongeun <hhg7899@gmail.com>
2023-07-25 12:48:04 +0900

    Android Gallery 연동

--- Boot07_Final/src/main/java/com/example/boot07/android/AndroidController.java
+++ Boot07_Final/src/main/java/com/example/boot07/android/AndroidController.java
@@ -2,6 +2,8 @@ package com.example.boot07.android;
 
 import com.example.boot07.cafe.dao.CafeDao;
 import com.example.boot07.cafe.dto.CafeDto;
+import com.example.boot07.gallery.dao.GalleryDao;
+import com.example.boot07.gallery.dto.GalleryDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,20 @@ import java.util.Map;
 // 컨트롤러 메소드에서 리턴하는 데이터가 바로 클라이언트에게 응답되는 컨트롤러
 @RestController
 public class AndroidController {
+
+    @Autowired
+    private GalleryDao galleryDao;
+
+    @GetMapping("/android/gallery/list")
+    public List<GalleryDto> galleryList() {
+        // 20개만 select 해오도록 GalleryDto 에 정보 담기
+        GalleryDto dto = new GalleryDto();
+        dto.setStartRowNum(1);
+        dto.setEndRowNum(20);
+        // GalleryDao 객체가 리턴해주는 데이터를 바로 리턴해주기
+        return galleryDao.getList(dto);
+    }
+
     /*
         String type 을 리턴하면 리턴된 String 의 내용이 그대로 응답된다.
      */
