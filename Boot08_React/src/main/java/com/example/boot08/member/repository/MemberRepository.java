8a2aa7e6fa54d4f84c181eed5fa26116e3915ce4
hyeongeun <hhg7899@gmail.com>
2023-09-05 09:33:18 +0900

    MEMBER API 완성

--- Boot08_React/src/main/java/com/example/boot08/member/repository/MemberRepository.java
+++ Boot08_React/src/main/java/com/example/boot08/member/repository/MemberRepository.java
@@ -2,11 +2,21 @@ package com.example.boot08.member.repository;
 
 import com.example.boot08.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-/*
-    Member Entity 에 대해서 insert, update, delete, select 작업을 대신해 줄
-    Repository 만들기
 
-    extends JpaRepository<Entity type, id 역할을 하는 필드의 type>
+import java.util.List;
+
+/*
+ *  Member Entity 에 대해서 insert, update, delete , select 작업을 대신해줄
+ *  Repository 만들기
+ *
+ *  extends JpaRepository<Entity type, id 역활을 하는 필드의 type>
  */
 public interface MemberRepository extends JpaRepository<Member, Integer> {
+    /*
+     *  정렬해서 select 하는 메소드를 custom 으로 추가하기
+     *  findAllByOrderBy칼럼명Desc()
+     *  findAllByOrderBy칼럼명Asc()
+     *  칼럼명은 camel case 로 작성
+     */
+    public List<Member> findAllByOrderByNumDesc();
 }
